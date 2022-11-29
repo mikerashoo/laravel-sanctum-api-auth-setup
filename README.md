ab2136651fb8ef3dc2d48470a1649b2067018ad3
Mikias Birhanu <37136032+mikerashoo@users.noreply.github.com>
2022-11-29 21:36:51 +0300

    basic login and registration features done

---
+++ README.md
@@ -0,0 +1,11 @@
+## JWT based authentication using laravel sanctum api for SPA and mobile applications
+ 
+Laravel is accessible, powerful, and provides tools required for large, robust applications.
+
+## Getting started
+# install laravel
+# setup env
+# install sanctum api 
+
+
+  
---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+         // Validate request data
+         $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:10',
+        ]);
+        // Return errors if validation error occur.
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json(['error' => $errors ],
+            400);
+        }
+        // Check if validation pass then create user and auth token. Return the auth token
+        if ($validator->passes()) {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user,
+
+            ]);
+        }
+    }
+
+    public function login(Request $request){
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid login details'], 401);
+        }
+        $user = User::where('emails', $request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json(['access_token' => $token,
+            'token_type' => 'Bearer',
+                'user' => $user,
+
+        ]);
+    }
+
+    public function me(Request $request){
+        return $request->user();
+    }
+}

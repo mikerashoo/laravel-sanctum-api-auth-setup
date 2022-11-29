ed6e9ff5844cc31ba8e51ebe78a524992f315844
Mikias Birhanu <37136032+mikerashoo@users.noreply.github.com>
2022-11-29 22:24:54 +0300

    redesigning database for role support

---
+++ database/migrations/2022_11_29_185504_create_roles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        DB::table('roles')->insert([
+            'name' => 'SUPER_ADMIN',
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'ADMIN',
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'CUSTOMER',
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+};
--- database/migrations/2022_11_29_185632_create_users_table.php
+++ database/migrations/2022_11_29_185632_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('role_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });

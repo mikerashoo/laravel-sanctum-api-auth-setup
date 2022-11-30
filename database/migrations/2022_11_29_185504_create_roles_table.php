d9bc1ade3466a9774e3d46c9057624079a386a2a
Mikias Birhanu <37136032+mikerashoo@users.noreply.github.com>
2022-11-30 19:02:58 +0300

    user and role table update

--- database/migrations/2022_11_29_185504_create_roles_table.php
+++ database/migrations/2022_11_29_185504_create_roles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,18 +17,24 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->boolean('is_editable')->default(true);
             $table->timestamps();
         });
 
         DB::table('roles')->insert([
             'name' => 'SUPER_ADMIN',
+            'is_editable'=> false,
         ]);
         DB::table('roles')->insert([
             'name' => 'ADMIN',
+            'is_editable'=> false,
+
         ]);
 
         DB::table('roles')->insert([
             'name' => 'CUSTOMER',
+            'is_editable'=> false,
+
         ]);
     }
 
--- database/migrations/2022_11_29_185632_create_users_table.php
+++ database/migrations/2022_11_29_185632_create_users_table.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -24,6 +26,13 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::table('users')->insert([
+            'name' => 'Mikiyas Birhanu',
+            'email' => 'mkbirhanu@gmail.com',
+            'role_id' => 1,
+            'password' => Hash::make('12345678')
+        ]);
     }
 
     /**
